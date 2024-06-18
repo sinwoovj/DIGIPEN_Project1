604982655f2f88d4d53d5b8b0318a24510562a74
Danhong <space12590@gmail.com>
2024-06-18 10:11:44 +0900

    Branch Test

--- Processing_Empty/Roughly.h
+++ Processing_Empty/Roughly.h
@@ -1,9 +1,7 @@
 #pragma once
 #include "cprocessing.h"
+#include "Projectile.h"
 #include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#define MAX_PROJECTILES 100
 
 typedef struct Rect
 {
@@ -15,26 +13,8 @@ typedef struct Rect
 R Rect;
 int aoc;
 
-typedef struct Projectile
-{
-	//투사체 사이즈
-	float projectile_size;
-
-	//투사체 위치 (처음 위치는 플레이어 위치일 예정)
-	float projectile_position_x;
-	float projectile_position_y;
-
-	//투사체 속도
-	float projectile_velocity;
-
-	int active;
-}Proj;
-Proj test[MAX_PROJECTILES];
-
 void Draw();
 void Key();
-
-void Create_Projectile(float x, float y, float velocity);
 
 void Roughly_game_init(void);
 void Roughly_game_update(void);
